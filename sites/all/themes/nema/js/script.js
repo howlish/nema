009353f35b869f6d06089c40fbe60d9d5146ccf9
Steve Polito <steve@shieldsdesignstudio.com>
2013-10-02 22:27:58 -0400

    Added form focus script.

--- sites/all/themes/nema/js/script.js
+++ sites/all/themes/nema/js/script.js
@@ -17,9 +17,54 @@
 Drupal.behaviors.my_custom_behavior = {
   attach: function(context, settings) {
 
+    //REMOVE TEXT FROM SOCIAL NAV
     $(document).ready(function(){
     	$('.sf-menu-social-nav a').empty();
     });
+
+    // FORM ON FOCUS//
+	var active_color = '#363636'; // Colour of user provided text
+	var inactive_color = '#363636'; // Colour of default text
+
+	jQuery(document).ready(function() {
+	  jQuery("input.form-text").css("color", inactive_color);
+	  var default_values = new Array();
+	  jQuery("input.form-text").focus(function() {
+	    if (!default_values[this.id]) {
+	      default_values[this.id] = this.value;
+	    }
+	    if (this.value == default_values[this.id]) {
+	      this.value = '';
+	      this.style.color = active_color;
+	    }
+	    jQuery(this).blur(function() {
+	      if (this.value == '') {
+	        this.style.color = inactive_color;
+	        this.value = default_values[this.id];
+	      }
+	    });
+	  });
+	});
+
+	jQuery(document).ready(function() {
+	  jQuery("textarea.form-textarea").css("color", inactive_color);
+	  var default_values = new Array();
+	  jQuery("textarea.form-textarea").focus(function() {
+	    if (!default_values[this.id]) {
+	      default_values[this.id] = this.value;
+	    }
+	    if (this.value == default_values[this.id]) {
+	      this.value = '';
+	      this.style.color = active_color;
+	    }
+	    jQuery(this).blur(function() {
+	      if (this.value == '') {
+	        this.style.color = inactive_color;
+	        this.value = default_values[this.id];
+	      }
+	    });
+	  });
+	}); 
 
   }
 };
